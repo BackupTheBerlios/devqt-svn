1d5fe6ef9a692f7fb35a3ffdc528224dd287a3ad
elcuco <elcuco@4e906b16-5b0d-0410-9381-8ed503f760cb>
2006-02-24 17:19:23 +0000

    added translations... oops... knew I was forgetting something :)

---
+++ trunk/translations/fr_FR.ts
@@ -0,0 +1,409 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>AbstractFile</name>
+    <message>
+        <source>Re&amp;move</source>
+        <translation>Re&amp;tirer</translation>
+    </message>
+    <message>
+        <source>&amp;Rename</source>
+        <translation>&amp;Renommer</translation>
+    </message>
+</context>
+<context>
+    <name>DevDirectory</name>
+    <message>
+        <source>scope : </source>
+        <translation>cible :</translation>
+    </message>
+</context>
+<context>
+    <name>DevDock</name>
+    <message>
+        <source>Movable</source>
+        <translation>Déplaçable</translation>
+    </message>
+    <message>
+        <source>Floatable</source>
+        <translation>Détachable</translation>
+    </message>
+    <message>
+        <source>Floating</source>
+        <translation>Détachée</translation>
+    </message>
+    <message>
+        <source>Allow on Left</source>
+        <translation>Autoriser à gauche</translation>
+    </message>
+    <message>
+        <source>Allow on Right</source>
+        <translation>Autoriser à droite
+</translation>
+    </message>
+    <message>
+        <source>Allow on Top</source>
+        <translation>Autoriser en haut</translation>
+    </message>
+    <message>
+        <source>Allow on Bottom</source>
+        <translation>Autoriser en bas</translation>
+    </message>
+    <message>
+        <source>Place on Left</source>
+        <translation>Placer à gauche</translation>
+    </message>
+    <message>
+        <source>Place on Right</source>
+        <translation>Placer à droite</translation>
+    </message>
+    <message>
+        <source>Place on Top</source>
+        <translation>Placer en haut</translation>
+    </message>
+    <message>
+        <source>Place on Bottom</source>
+        <translation>Placer en bas</translation>
+    </message>
+</context>
+<context>
+    <name>DevFile</name>
+    <message>
+        <source>file : </source>
+        <translation>fichier :</translation>
+    </message>
+</context>
+<context>
+    <name>DevFolder</name>
+    <message>
+        <source>&amp;Subdir</source>
+        <translation>&amp;Sous dossier</translation>
+    </message>
+    <message>
+        <source>&amp;New file</source>
+        <translation>&amp;Nouveau Fichier</translation>
+    </message>
+    <message>
+        <source>folder : </source>
+        <translation>dossier :</translation>
+    </message>
+</context>
+<context>
+    <name>DevGUI</name>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>&amp;Refaire</translation>
+    </message>
+    <message>
+        <source>Cu&amp;t</source>
+        <translation>Co&amp;uper</translation>
+    </message>
+    <message>
+        <source>Cop&amp;y</source>
+        <translation>Cop&amp;ier</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translation>Co&amp;ller</translation>
+    </message>
+    <message>
+        <source>&amp;Select All</source>
+        <translation>Tout &amp;sélectionner</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>Effacer la sélec&amp;tion</translation>
+    </message>
+    <message>
+        <source>&amp;Find</source>
+        <translation>C&amp;hercher</translation>
+    </message>
+    <message>
+        <source>Find &amp;Next</source>
+        <translation>Chercher suiva&amp;nt</translation>
+    </message>
+    <message>
+        <source>R&amp;eplace</source>
+        <translation>R&amp;emplacer</translation>
+    </message>
+    <message>
+        <source>&amp;Goto</source>
+        <translation>Alle&amp;r à</translation>
+    </message>
+    <message>
+        <source>Toggle Brea&amp;kpoint</source>
+        <translation>Activer brea&amp;kpoint</translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation>Imprimer</translation>
+    </message>
+    <message>
+        <source>Pr&amp;operties</source>
+        <translation>Pr&amp;opriété</translation>
+    </message>
+    <message>
+        <source>File Actions</source>
+        <translation>Actions sur les fichiers</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nouveau</translation>
+    </message>
+    <message>
+        <source>New &amp;project</source>
+        <translation>Nouveau &amp;projet</translation>
+    </message>
+    <message>
+        <source>&amp;Source file</source>
+        <translation>Fichier &amp;source</translation>
+    </message>
+    <message>
+        <source>&amp;User Interface</source>
+        <translation>Interface &amp;Utilisateur (UI)</translation>
+    </message>
+    <message>
+        <source>&amp;Resource</source>
+        <translation>&amp;Ressource (QRC)</translation>
+    </message>
+    <message>
+        <source>&amp;Template</source>
+        <translation>&amp;Modèle de projet</translation>
+    </message>
+    <message>
+        <source>&amp;Open...</source>
+        <translation type="obsolete">&amp;Ouvrir...</translation>
+    </message>
+    <message>
+        <source>&amp;Reopen...</source>
+        <translation type="obsolete">&amp;Réouvrir...</translation>
+    </message>
+    <message>
+        <source>&amp;Save...</source>
+        <translation>&amp;Sauvegarder...</translation>
+    </message>
+    <message>
+        <source>Sa&amp;ve All...</source>
+        <translation>Tout sau&amp;vegarder...</translation>
+    </message>
+    <message>
+        <source>Save &amp;As...</source>
+        <translation>S&amp;auvegarder sous...</translation>
+    </message>
+    <message>
+        <source>Save Project</source>
+        <translation>Sauvegarder projet</translation>
+    </message>
+    <message>
+        <source>Save project As...</source>
+        <translation>Sauvegarder project sous...</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>Fe&amp;rmer</translation>
+    </message>
+    <message>
+        <source>C&amp;lose all</source>
+        <translation>Fermer &amp;tout</translation>
+    </message>
+    <message>
+        <source>Close projec&amp;t</source>
+        <translation>Fermer pro&amp;jet</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <source>Edit Actions</source>
+        <translation>Actions d&apos;édition</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Edition</translation>
+    </message>
+    <message>
+        <source>Search Actions</source>
+        <translation>Actions de recherche</translation>
+    </message>
+    <message>
+        <source>&amp;Search</source>
+        <translation>Recherc&amp;her</translation>
+    </message>
+    <message>
+        <source>Files</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <source>Classes</source>
+        <translation>Classes</translation>
+    </message>
+    <message>
+        <source>Compiler Actions</source>
+        <translation>Actions de compilation</translation>
+    </message>
+    <message>
+        <source>&amp;Compile</source>
+        <translation type="obsolete">&amp;Compiler</translation>
+    </message>
+    <message>
+        <source>&amp;Compile...</source>
+        <translation>&amp;Compiler...</translation>
+    </message>
+    <message>
+        <source>&amp;Run...</source>
+        <translation>&amp;Executer...</translation>
+    </message>
+    <message>
+        <source>&amp;Params</source>
+        <translation>&amp;Paramètres</translation>
+    </message>
+    <message>
+        <source>C&amp;ompile and run...</source>
+        <translation>C&amp;ompiler et exécuter...</translation>
+    </message>
+    <message>
+        <source>R&amp;ebuild all...</source>
+        <translation>Tout &amp;reconstruire...</translation>
+    </message>
+    <message>
+        <source>&amp;Syntax check...</source>
+        <translation>Vérification &amp;syntaxique...</translation>
+    </message>
+    <message>
+        <source>C&amp;lean...</source>
+        <translation>&amp;Nettoyer...</translation>
+    </message>
+    <message>
+        <source>Compiler</source>
+        <translation>Compilateur</translation>
+    </message>
+    <message>
+        <source>Projects Actions</source>
+        <translation>Actions sur le projet</translation>
+    </message>
+    <message>
+        <source>Help Actions</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <source>A&amp;bout Qt...</source>
+        <translation>À propos de &amp;Qt...</translation>
+    </message>
+    <message>
+        <source>Qt Assistant...</source>
+        <translation>Assistant Qt...</translation>
+    </message>
+    <message>
+        <source>&amp;About DevQt...</source>
+        <translation>&amp;À propos de DevQt...</translation>
+    </message>
+    <message>
+        <source>&amp;DevQt Help...</source>
+        <translation>Aide de &amp;DevQt...</translation>
+    </message>
+    <message>
+        <source>E&amp;xecute</source>
+        <translation>E&amp;xécution</translation>
+    </message>
+    <message>
+        <source>&amp;Open project or file...</source>
+        <translation>&amp;Ouvrir projet ou fichier...</translation>
+    </message>
+    <message>
+        <source>&amp;Project</source>
+        <translation>&amp;Projet</translation>
+    </message>
+    <message>
+        <source>Scopes Actions</source>
+        <translation>Cibles de compilations</translation>
+    </message>
+    <message>
+        <source>&amp;Tools</source>
+        <translation>Ou&amp;tils</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Configuration</translation>
+    </message>
+</context>
+<context>
+    <name>DevProject</name>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>DevSettings</name>
+    <message>
+        <source>&amp;Reopen...</source>
+        <translation>&amp;Réouvrir...</translation>
+    </message>
+    <message>
+        <source>&amp;Default</source>
+        <translation>&amp;Defaut</translation>
+    </message>
+    <message>
+        <source>&amp;Ok</source>
+        <translation>&amp;Valider</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+    <message>
+        <source>&amp;Clear history</source>
+        <translation>&amp;Nettoyer l&apos;historique</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <source>Compilation</source>
+        <translation>Compilation</translation>
+    </message>
+    <message>
+        <source>Editors</source>
+        <translation>Editeurs</translation>
+    </message>
+    <message>
+        <source>Highlighting</source>
+        <translation>Coloration</translation>
+    </message>
+</context>
+<context>
+    <name>DevStatus</name>
+    <message>
+        <source>Explorer</source>
+        <translation>Explorateur</translation>
+    </message>
+    <message>
+        <source>Compiler</source>
+        <translation>Compilateur</translation>
+    </message>
+</context>
+<context>
+    <name>DevWorkSpace</name>
+    <message>
+        <source>WorkSpace : </source>
+        <translation>Espace de travail :</translation>
+    </message>
+    <message>
+        <source>External</source>
+        <translation>Externe</translation>
+    </message>
+</context>
+</TS>
